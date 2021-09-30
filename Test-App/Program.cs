e16ff7f1cfe8d3e96f68c895199ce8ef6c84747a
Link4real <simon@die-duerrs.de>
2021-09-30 16:58:27 +0200

    guess the link

--- Test-App/Program.cs
+++ Test-App/Program.cs
@@ -6,16 +6,33 @@ namespace Test_App
     {
         static void Main(string[] args)
         {
+            string secretWord = "Link";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool OutOfGuesses = false;
 
-            int index = 6;
             do
             {
-                Console.WriteLine(index);
-                index++;
-            } while (index <= 5);
-
-
-                Console.ReadLine();
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    OutOfGuesses = true;
+                }
+            } while (secretWord != guess && !OutOfGuesses);
+            if (OutOfGuesses)
+            {
+                Console.WriteLine("You Lose!");
+            } else
+            {
+                Console.WriteLine("You Win!");
+            }
+            Console.ReadLine();
         }
     }
 }
